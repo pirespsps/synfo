7484a276adda31c581b5553f54154ed032193b1a
pirespsps <matheusknoll28@gmail.com>
2026-01-20 22:26:03 -0300

    system host e dns

--- model/network.go
+++ model/network.go
@@ -13,8 +13,9 @@ type Network struct {
 }
 
 type NetworkData struct {
-	SystemHost string `json:"system"`
-	Hosts      []Host `json:"hosts"`
+	SystemHost string   `json:"system"`
+	Hosts      []Host   `json:"hosts"`
+	DNS        []string `json:"dns"` //first 3 dns services (linux max)
 }
 
 type Host struct {
@@ -29,7 +30,7 @@ func (n *Network) Overall() ([]byte, error) {
 		return nil, fmt.Errorf("error in load: %v", err)
 	}
 
-	by, err := json.Marshal(n)
+	by, err := json.Marshal(n.Data)
 	if err != nil {
 		return nil, fmt.Errorf("error in marshal: %v", by)
 	}
@@ -44,7 +45,7 @@ func (n *Network) Extensive() ([]byte, error) {
 		return nil, fmt.Errorf("error in load: %v", err)
 	}
 
-	by, err := json.Marshal(n)
+	by, err := json.Marshal(n.Data)
 	if err != nil {
 		return nil, fmt.Errorf("error in marshal: %v", by)
 	}
@@ -66,7 +67,50 @@ func (n *Network) Load() error {
 		return fmt.Errorf("error in hosts: %v", err)
 	}
 
+	n.Data.SystemHost, err = getHostName()
+	if err != nil {
+		return fmt.Errorf("error in hostname: %v", err)
+	}
+
+	n.Data.DNS, err = getDNSs()
+	if err != nil {
+		return fmt.Errorf("error in dns: %v", err)
+	}
+
 	return nil
+}
+
+func getHostName() (string, error) {
+	name, err := os.ReadFile("/etc/hostname")
+	if err != nil {
+		return "", fmt.Errorf("error reading file: %v", err)
+	}
+
+	return strings.TrimSpace(string(name)), nil
+}
+
+func getDNSs() ([]string, error) {
+	var dnss []string
+
+	data, err := os.ReadFile("/etc/resolv.conf")
+	if err != nil {
+		return nil, fmt.Errorf("error in reading file: %v", err)
+	}
+
+	for line := range strings.Lines(string(data)) {
+		if len(dnss) == 3 {
+			break
+		}
+
+		line = strings.TrimSpace(line)
+
+		if strings.HasPrefix(line, "nameserver") {
+			_, after, _ := strings.Cut(line, "nameserver")
+			dnss = append(dnss, strings.TrimSpace(after))
+		}
+	}
+
+	return dnss, nil
 }
 
 func getHosts() ([]Host, error) {
